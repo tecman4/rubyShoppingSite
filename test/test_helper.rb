d6284f67a75e1d6ce8bd460793b7bf14cf8ab4d6
Wayne <tecman4@gmail.com>
2023-10-08 19:32:10 -0400

    Initial Commit

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+class ActionDispatch::IntegrationTest
+def login_as(user)
+post login_url, params: { name: user.name, password: 'secret' }
+end
+def logout
+delete logout_url
+end
+def setup
+login_as users(:one)
+end
+end
